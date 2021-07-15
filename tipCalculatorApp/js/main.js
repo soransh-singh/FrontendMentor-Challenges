1deb2dca13e86f0de6266e1a21aaa26a810d7892
soransh <57911107+soransh-singh@users.noreply.github.com>
2021-07-15 17:28:50 +0530

    tip calculator app update

--- tipCalculatorApp/js/main.js
+++ tipCalculatorApp/js/main.js
@@ -1,3 +1,77 @@
 window.addEventListener("load", ()=>{
-  console.log("window loaded")
+
+  const amount = document.querySelector("#amount")
+
+  /* perc : percentage */
+  const percBtns = document.querySelectorAll(".percentage-btn")
+  const customPerc = document.querySelector(".custom-percentage")
+
+  const numOfPeople = document.querySelector('#no-people')
+
+  const tipAmountRender = document.querySelector(".tip-amount")
+  const perPersonRender = document.querySelector(".per-person")
+
+  const reset = document.querySelector(".reset")
+  console.log(reset);
+
+  let varAmount = 0
+  let varNumOfPeople = 1
+  let varTipPercentage = 0
+
+  tipAmountRender.textContent = `$00.00`
+  perPersonRender.textContent = `$00.00`
+
+  function calc() {
+    const tipTotal = (varAmount*varTipPercentage)/100
+    tipAmountRender.textContent = `$${tipTotal.toFixed(2)}`
+    if(varNumOfPeople !==0){
+      const perPerson = (varAmount + tipTotal)/varNumOfPeople
+      perPersonRender.textContent = `$${perPerson.toFixed(2)}`
+    }else{
+      perPersonRender.textContent = `$00.00`
+    }
+  }
+  // const total = Math.round(varAmount*varTipPercentage)/100
+  //`${Math.round(total)}.${Math.}`
+
+  amount.addEventListener("input", ()=>{
+    varAmount = Number(amount.value)
+    console.log(varAmount)
+    calc()
+  })
+
+  percBtns.forEach((btn) => {
+    btn.addEventListener("click", ()=>{
+      varTipPercentage = Number(btn.dataset["percentage"])
+      console.log(varTipPercentage)
+      calc()
+    })
+  })
+
+  customPerc.addEventListener("input", ()=>{
+    varTipPercentage = Number(customPerc.value)
+    console.log(varTipPercentage)
+    calc()
+  })
+
+  numOfPeople.addEventListener("input", ()=>{
+    varNumOfPeople = Number(numOfPeople.value)
+    console.log(numOfPeople.value);
+    calc()
+  })
+
+  reset.addEventListener("click", ()=>{
+    varAmount = 0
+    varNumOfPeople = 1
+    varTipPercentage = 0
+
+    tipAmountRender.textContent = `$00.00`
+    perPersonRender.textContent = `$00.00`
+
+    amount.value =""
+    customPerc.value =""
+    numOfPeople.value ="1"
+  })
+
+
 })
